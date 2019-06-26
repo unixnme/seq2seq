330f98a785b94279042a4731d58f15c43de28c6c
Unix&Me <unixnme@gmail.com>
2019-06-26 12:28:57 -0700

    add test script

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ def main():
     parser.add_argument('--input', type=int, nargs='+', required=True)
     args = parser.parse_args()
 
+    if 0 in args.input:
+        print('0 cannot be allowed in the input')
+        exit(-1)
+
     network = torch.load(args.load, map_location='cpu')
     out = network.generate(args.input, args.max_length)
     print(out)
