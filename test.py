70fe51cb0463c33e1f8d0f4cae22a09b67bc9d48
Unix&Me <unixnme@gmail.com>
2019-06-26 12:25:06 -0700

    add test script

---
+++ test.py
@@ -0,0 +1,18 @@
+import argparse
+import torch
+
+
+def main():
+    parser = argparse.ArgumentParser('Test Seq2Seq')
+    parser.add_argument('--load', required=True, type=str)
+    parser.add_argument('--max_length', type=int, default=100)
+    parser.add_argument('--input', type=int, nargs='+', required=True)
+    args = parser.parse_args()
+
+    network = torch.load(args.load, map_location='cpu')
+    out = network.generate(args.input, args.max_length)
+    print(out)
+
+
+if __name__ == '__main__':
+    main()
